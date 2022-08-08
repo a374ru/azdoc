e92064f31cca0c0b9d08d7cd85bed77356388572
Oleg <82386871+a374ru@users.noreply.github.com>
2022-08-08 11:35:06 +0300

    777

--- docs/assets/js/navi.js
+++ docs/assets/js/navi.js
@@ -151,7 +151,7 @@ function toNavi() {
 
 	cday = new Date().getDate();
 
-	htmlString += `<div class="navi-item" id="day"><a href="dobro-day"><span id="dobro-day">День</span></a><a href="https://a374ru.github.io/aprakos.ru/currentday/APRAKOS/index.html"><span class="number-day" id="number-day">${cday}</span></a></div><div class="navi-item" title="ПОИСК И НАВИГАЦИЯ" id="navi-page-search"><a href="navi-page#navi">√</a></div><div class="navi-item" title="Цветность" id="colorScheme"><a onclick="clickColor()">
+	htmlString += `<div class="navi-item" id="day"><a href="https://a374ru.github.io/aprakos.ru/"><span id="dobro-day">День </span></a><a href="https://a374ru.github.io/aprakos.ru/currentday/APRAKOS/index.html"><span class="number-day" id="number-day">${cday}</span></a></div><div class="navi-item" title="ПОИСК И НАВИГАЦИЯ" id="navi-page-search"><a href="navi-page#navi">√</a></div><div class="navi-item" title="Цветность" id="colorScheme"><a onclick="clickColor()">
 <span class="material-icons-two-tone">
 light_mode
 </span></a></div>`
