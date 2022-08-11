7d28cbebca92b76b7c9b44c50105af05640eaaf1
Oleg <82386871+a374ru@users.noreply.github.com>
2022-08-11 17:52:12 +0300

    777

--- docs/assets/js/navi.js
+++ docs/assets/js/navi.js
@@ -28,7 +28,7 @@ document.addEventListener('keyup', function (event) {
 // Проверка и установка стартового значения
 if (localStorage.getItem("az") != 1) {
 
-	localStorage.setItem("color", '#fffaf5');
+	localStorage.setItem("color", '#fffbf6');
 }
 
 
@@ -36,9 +36,9 @@ if (localStorage.getItem("az") != 1) {
 function switchColorScheme() {
 
 
-	whiteBG = '#fffaf5';
+	whiteBG = '#fffbf6';
 	darkBG = '#110000';
-	naviLight = `box-shadow: 0px 10 22 #fffaf5; background:linear-gradient(180deg, ${whiteBG} 0%, ##A78F5B 3%)`;
+	naviLight = `box-shadow: 0px 10 22 #fffbf6; background:linear-gradient(180deg, ${whiteBG} 0%, ##A78F5B 3%)`;
 	naviDark = `box-shadow: 0px 20px 52px #611816; background:linear-gradient(180deg, ${darkBG} 97%, red 1%)`;
 
 	trgr = localStorage.getItem('color') == whiteBG ? true : false;
