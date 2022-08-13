3878381f9b238bae18a58d591b2e871a76704782
Oleg <82386871+a374ru@users.noreply.github.com>
2022-08-13 11:55:12 +0300

    777

--- docs/assets/js/navi.js
+++ docs/assets/js/navi.js
@@ -37,7 +37,7 @@ function switchColorScheme() {
 
 	whiteBG = '#fff';
 	darkBG = '#110000';
-	naviLight = `box-shadow: 0px 10 22 #fff; background:linear-gradient(180deg, ${whiteBG} 93%, #8d6e63 97%)`;
+	naviLight = `box-shadow: 0px 10 22 #fff; background:linear-gradient(180deg, ${whiteBG} 97%, #8d6e63 1%)`;
 	naviDark = `box-shadow: 0px 20px 52px #611816; background:linear-gradient(180deg, ${darkBG} 97%, red 1%)`;
 
 	trgr = localStorage.getItem('color') == whiteBG ? true : false;
